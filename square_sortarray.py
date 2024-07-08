56790f8c69f296518a3ee11aad89e09ecf215ce8
aiyan1234 <143414484+aiyan1234@users.noreply.github.com>
2024-07-08 23:37:37 +0530

    Add files via upload

---
+++ square_sortarray.py
@@ -0,0 +1,46 @@
+'''977. Squares of a Sorted Array
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+ 
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?'''
+
+def sortedSquares(nums: list[int]) -> list[int]:
+        nums[:]=[i**2 for i in nums]
+        if len(nums) == 0 or len(nums)==1:
+            return nums
+        i=0
+        j=len(nums)-1
+        p=len(nums)-1
+        res=[0]*(len(nums))
+        while i<=j:
+            if nums[i]>nums[j]:
+                res[p]=nums[i]
+                i+=1
+            else:
+                res[p]=nums[j]
+                j-=1
+            p-=1
+        print(res)
+        return res
+nums=[-5,-4,-3,5,7]
+sortedSquares(nums)
