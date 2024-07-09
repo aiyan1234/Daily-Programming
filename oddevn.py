5e8f5c60986a14a888c312aa24e5b38dbc88012c
aiyan1234 <143414484+aiyan1234@users.noreply.github.com>
2024-07-09 22:23:09 +0530

    Add files via upload

---
+++ oddevn.py
@@ -0,0 +1,5 @@
+n=int(input("enter the number"))
+if n&1==0:
+    print("even") # Checks if the last bit of n is 0 (even number)
+else:
+    print('odd')  # If the last bit of n is 1, it's an odd number
