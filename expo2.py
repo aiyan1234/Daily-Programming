72df73d3c0409b0108c5441e289fc70d282d8b7d
aiyan1234 <143414484+aiyan1234@users.noreply.github.com>
2024-08-16 23:25:00 +0530

    Add files via upload

---
+++ expo2.py
@@ -0,0 +1,50 @@
+'''Problem Statement:
+You are given a function:
+Int MaxExponents (int a , int b);
+You have to find and return the number between ‘a’ and ‘b’ ( range inclusive on both ends) which has the maximum exponent of 2.
+The algorithm to find the number with maximum exponent of 2 between the given range is
+
+Loop between ‘a’ and ‘b’. Let the looping variable be ‘i’.
+Find the exponent (power) of 2 for each ‘i’ and store the number with maximum exponent of 2 so far in a variable , let say ‘max’. Set ‘max’ to ‘i’ only if ‘i’ has more exponent of 2 than ‘max’.
+Return ‘max’.
+
+Assumption: a < b
+Note: If two or more numbers in the range have the same exponents of 2 , return the small number.
+
+Example
+Input:
+7
+12
+
+Output:
+8
+
+Explanation:
+Exponents of 2 in:
+7-0
+8-3
+9-0
+10-1
+11-0
+12-2
+Hence the maximum exponent if two is of 8.'''
+def expo(num):
+    exp=0
+    while num%2==0 and num!=0:
+        exp+=1
+        num//=2
+    return exp
+def rangeexpo(a,b):
+    maxexpo=0
+    numb=a
+    for i in range(a,b+1):
+        temp=expo(i)
+        if temp>maxexpo:
+            maxexpo=temp
+            numb=i
+    return numb
+
+
+a=int(input())
+b=int(input())
+print(rangeexpo(a,b))
