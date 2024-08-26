73926d53f9be4273a4642c2c6fe6243a407d791e
aiyan1234 <143414484+aiyan1234@users.noreply.github.com>
2024-08-26 11:27:31 +0530

    Add files via upload

---
+++ sum_3_5.py
@@ -0,0 +1,27 @@
+'''You are required to implement the following function:
+Int Calculate(int m, int n);
+The function accepts 2 positive integers ‘m’ and ‘n’ as its arguments.You are required to calculate the sum of numbers divisible both by 3 and 5, between ‘m’ and ‘n’ both inclusive and return the same.
+
+Note:
+0 < m <= n
+
+Example
+Input:
+m : 12
+n : 50
+
+Output
+90
+
+Explanation:
+The numbers divisible by both 3 and 5, between 12 and 50 both inclusive are {15, 30, 45} and their sum is 90.'''
+
+def sum_of_div(m,n):
+    s=0
+    for i in range(m,n+1):
+        if i%3==0 and i%5==0:
+            s+=i
+    return s
+m=int(input())
+n=int(input())
+print(sum_of_div(m,n))      
